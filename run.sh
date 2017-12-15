5ab7380c88dac7f3045135fdb9c76cd1b054035b
Sergio Valverde <sergio.valverde@imdea.org>
2017-12-15 08:47:25 +0100

    AoD commit

--- run.sh
+++ run.sh
@@ -1,35 +1,89 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-#if [ $# -ne 1 ] || [ $1 -lt 1 ] || [ $1 -gt 3 ]
-#then
-#  echo "Usage: $0 level[1-3]"
-#  exit
-#fi
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <app_apk_path> <time_to_run_puma_in_min>"
+  echo "Example: $0 \"/home/vagrant/subjects/a2dp.Vol_93_src\" \"10\""
+  exit
+fi
+
+app_apk_path=$1
+if [[  ! -e ${app_apk_path} ]]; then
+  echo "This app folder: ${app_apk_path} - Doesn't exist or match with more than 1"
+  echo "Run again with a correct folder name."
+  exit 1
+fi
+
+time=$2
+if [[  -z ${time} ]]; then
+  echo "This var: time - Not setted"
+  echo "Run again passing the correct time number"
+  exit 1
+fi
 
 
-APP=`head -1 app.info`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+APPINFO=${DIR}/app.info
 UID_FN=/data/local/tmp/app.uid
+UID_FN_BackUp=${DIR}/app.uid_BackUpFromEMU
 
-adb push app.info /data/local/tmp/
 
-# 0. Start app from fresh
-adb shell "am force-stop $APP"
 
-# 1. Find and save UID, if needed
-curr_uid=`adb shell dumpsys package $APP | grep userId | awk -F"=| " '{print $6}'`
+# get info from apk
+package=$(aapt d xmltree ${app_apk_path} AndroidManifest.xml | grep package | awk 'BEGIN {FS="\""}{print $2}')
+echo ${package} > ${APPINFO}
+appLabel=$(aapt d badging ${app_apk_path} | grep application-label: | awk -F\' '{print $2}')
+echo ${appLabel} >> ${APPINFO}
+echo ${time} >> ${APPINFO}
+
+
+CMD="adb push ${APPINFO} /data/local/tmp"
+echo "        * ${CMD}"
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1
+
+
+APP=$(head -1 ${APPINFO})
+
+
+echo "0. Start app from fresh"
+
+CMD="adb shell \"am force-stop ${APP}\""
+echo "        * ${CMD}"
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1
+
+
+echo "1. Find and save UID, if needed"
+
+curr_uid=`adb shell dumpsys package ${APP} | grep userId | awk -F"=| " '{print $6}'`
 old_uid=`adb shell "cat ${UID_FN}" | tr -d '\r\n'`
 
-if [ "${curr_uid}" != "${old_uid}" ]
-then
+if [ "${curr_uid}" != "${old_uid}" ]; then
   echo "Updating UID"
   adb shell "echo ${curr_uid} > ${UID_FN}"
-  adb shell "cat ${UID_FN}"
+  adb shell "cat ${UID_FN}"  >  ${UID_FN_BackUp}
 else
   echo "UID OK"
 fi
 
-# 2. Start command
-ant build
-adb push bin/TestApp.jar /data/local/tmp/
-adb shell /data/local/tmp/haos runtest TestApp.jar -c nsl.stg.tests.LaunchApp
 
+echo "2. Start command"
+
+CMD="ant build"
+echo "        * ${CMD}"
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1
+
+CMD="adb push ${DIR}/bin/TestApp.jar /data/local/tmp/"
+echo "        * ${CMD}"
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1
+
+CMD="adb shell /data/local/tmp/haos runtest TestApp.jar -c nsl.stg.tests.LaunchApp"
+echo "        * ${CMD}"
+eval ${CMD}
+[[ $? -ne 0 ]] && echo "ERROR" && exit 1
+
+exit 0
--- setup-phone.sh
+++ setup-phone.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-adb push haos /data/local/tmp
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+adb push ${DIR}/haos /data/local/tmp
 adb shell "chmod 0755 /data/local/tmp/haos"
 adb shell "ls -l /data/local/tmp/haos"
 
